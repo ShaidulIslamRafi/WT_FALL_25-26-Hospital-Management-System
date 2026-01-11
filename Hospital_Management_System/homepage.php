ccbacea24aa41627dc4699f78d2df8185ac614e5
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2026-01-12 02:20:33 +0600

    work on home page

---
+++ Hospital_Management_System/homepage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="css/home.css" >
+    </head>
+<body>
+
+<div class="one">
+<nav>
+  <a href="">Login</a>
+  <a href="">Sing Up</a>
+</nav>    
+</div><br> <br>
+<div class="two">
+    
+    <h1>Hospital Management System </h1>
+    <p>Comprehensiv tracking Doctors details </p>
+    <p>Set appointment with doctor</p>
+    <p>Store patient record</p>
+    <button name="login" onclick="loginPage.php">Login</button>
+    <button name="signup">Sign up</button>
+</div>
+
+
+</body>    
