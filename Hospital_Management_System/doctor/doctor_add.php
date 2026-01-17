9cee8f4b0463201b44e4e5e1fdc8d0e2f58367b9
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2026-01-17 23:11:50 +0600

    work on doctor_add.php

---
+++ Hospital_Management_System/doctor/doctor_add.php
@@ -0,0 +1,78 @@
+<?php
+   session_start();
+   if(!isset($_SESSION["username"])){
+    header("Location: ../loginPage.php");
+    exit;
+   }
+   include "../database.php";
+
+
+   $error="";
+    if(isset($_POST["save"])){
+
+        $username=trim($_POST["username"]);
+        $dname=trim($_POST["dname"]);
+        $phone=(int)trim($_POST["phone"]);
+        $specialist=trim($_POST["specialist"]);
+    if($username=="" || $dname=="" || $specialist=="" || $phone<=0 ){
+       $error="All field are requred ";
+    }
+        else{
+               $statement=$conn->prepare("INSERT INTO doctor (username, dname, phone, specialist) VALUES (?,?,?,?)");
+               $statement->bind_param("ssis",$username,$dname,$phone,$specialist);
+
+               if($statement->execute()){
+                header("Location:../doctorPage.php");
+                exit;
+               }
+               else{
+                $error="Insert Failed! Username may already exist.";
+               }
+               $statement->close();
+        }
+
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> </title>
+<link  rel="stylesheet" href="css/doctor.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+
+    <div class="topbar">
+          <h2>Add New Doctor</h2>
+          <a class="back_btn"href="../doctorPage.php">Back</a>
+    </div>
+<?php  if($error!="") echo"<P class='error_msg'>$error</p>"; ?>
+
+   <form method="post" class="form">
+
+     <label >Username</label>
+     <input type="text" name="username" required >
+
+      <label >Doctor Name</label>
+     <input type="text" name="dname" required >
+     
+     <label >Phone</label>
+     <input type="text" name="phone" required >
+
+     <label >Specialist</label>
+     <input type="text" name="specialist" required >
+
+     <button type="submit" name="save" class="add_btn">Save</button>
+
+
+
+
+
+
+   </form>
+
+
+</body>
+</html>
