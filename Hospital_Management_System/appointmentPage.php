e439465e0a28e0b1be6497c6fe87e448c0f2e0fe
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2026-01-18 19:33:50 +0600

    check

--- Hospital_Management_System/appointmentPage.php
+++ Hospital_Management_System/appointmentPage.php
@@ -1,4 +1,4 @@
-//change
+
 <?php
    session_start();
    if(!isset($_SESSION["username"])){
--- Hospital_Management_System/database.php
+++ Hospital_Management_System/database.php
@@ -1,5 +1,5 @@
 <?php
-    $host="localhost";
+    $host="localhost" ;
     $user="root";
     $pass="";
     $dbname="hospital_management_system";
